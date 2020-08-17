8e1adc449e15bf30db56b3883e7b476edd592899
ArmandoTapia <rodney.tapia@epn.edu.ec>
2020-08-17 08:58:57 -0500

    fix vug import

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -40,7 +59,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var mongoose_1 = __importDefault(require("mongoose"));
+var mongoose = __importStar(require("mongoose"));
 var Server = /** @class */ (function () {
     function Server() {
         this.app = express_1.default();
@@ -60,7 +79,7 @@ var Server = /** @class */ (function () {
                 switch (_a.label) {
                     case 0:
                         _a.trys.push([0, 2, , 3]);
-                        return [4 /*yield*/, mongoose_1.default.connect(
+                        return [4 /*yield*/, mongoose.connect(
                             // `mongodb://192.168.100.243:27017/database_tdl`,
                             "mongodb://Armandev:Armas1350_epis@ds137271.mlab.com:37271/todolegal", {
                                 useNewUrlParser: true,
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Application } from 'express';
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 class Server {
   private static _instance: Server;
   public app: Application;
