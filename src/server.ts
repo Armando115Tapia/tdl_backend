1ad1afae50ff25d7e25316ad5be2d3477c021244
ArmandoTapia <rodney.tapia@epn.edu.ec>
2020-08-17 11:58:01 -0500

    ADD some changes1

--- src/server.ts
+++ src/server.ts
@@ -15,10 +15,10 @@ class Server {
 
   private async connectToTheDatabase() {
     try {
-      
+
       const dataBaseconnect = await mongoose.connect(
-        `mongodb://192.168.100.243:27017/database_tdl`,
-        //`mongodb://Armandev:Armas1350_epis@ds137271.mlab.com:37271/todolegal`,
+        //`mongodb://192.168.100.243:27017/database_tdl`,
+        `mongodb://Armandev:Armas1350_epis@ds137271.mlab.com:37271/todolegal`,
         {
           useNewUrlParser: true,
           useFindAndModify: false,
